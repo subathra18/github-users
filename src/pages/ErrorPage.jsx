e051c61a6c00c04b8fc3f881617cd8c67d8a8640
subathra <subi@subathras-MacBook-Air.local>
2024-02-25 17:54:47 +0530

    added error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,10 +1,26 @@
 import React from "react";
-import { useRouteError } from "react-router-dom";
+import { useRouteError, Link } from "react-router-dom";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 
 const ErrorPage = () => {
   const error = useRouteError();
   console.log("error", error);
-  return <div>error page </div>;
+  return (
+    <>
+      <Typography component={"span"} variant="body2">
+        <Box style={{ fontSize: 15, padding: "10px" }}>
+          The page you are looking for is not available
+        </Box>
+      </Typography>
+      <Link className="link" to="/">
+        <Typography component={"span"} variant="body2">
+          <Box style={{ fontSize: 15, padding: "10px" }}>Go to Home</Box>
+        </Typography>
+      </Link>
+    </>
+  );
 };
 
 export default ErrorPage;
