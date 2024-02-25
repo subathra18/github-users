fabfbd7b00083728aff06b14cb1933a1a3c72fda
subathra <subi@subathras-MacBook-Air.local>
2024-02-25 17:59:27 +0530

    added error component

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { useRouteError } from "react-router-dom";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 
 const Error = () => {
   const error = useRouteError();
   console.log("error", error);
-  return <h1>There was an error</h1>;
+  return (
+    <Typography component={"span"} variant="body2">
+      <Box style={{ fontSize: 15, padding: "10px" }}>There was an error</Box>
+    </Typography>
+  );
 };
 
 export default Error;
